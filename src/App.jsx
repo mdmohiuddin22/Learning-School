c0d94b46ba80126c5d17e20c373cf6252b769b31
Md.Mohiuddin <saddammd203@gmail.com>
2023-06-10 23:20:30 +0600

    start client side

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h4>Hello Tailwind</h4>
+      <button className="btn">Button</button>
+<button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Button</button>
+<button className="btn btn-secondary">Button</button>
+<button className="btn btn-accent">Button</button>
+<button className="btn btn-ghost">Button</button>
+<button className="btn btn-link">Button</button>
+      
+      
+    </>
+  )
+}
+
+export default App
