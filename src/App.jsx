615809a5655cdff069d85926b9715ccafee7b5b4
Md.Mohiuddin <saddammd203@gmail.com>
2023-06-11 00:08:23 +0600

    added nav and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 
 import './App.css'
+import Footer from './Nav+Footer/Footer'
+import Navbar from './Nav+Footer/Navbar'
 
 function App() {
  
 
   return (
     <>
-      <h4>Hello Tailwind</h4>
-      <button className="btn">Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Button</button>
-<button className="btn btn-secondary">Button</button>
-<button className="btn btn-accent">Button</button>
-<button className="btn btn-ghost">Button</button>
-<button className="btn btn-link">Button</button>
-      
+<Navbar></Navbar>
+<Footer></Footer>
+
+
       
     </>
   )
