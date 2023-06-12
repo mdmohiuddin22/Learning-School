0a4508709e28f31c2a9ab1a419cc9cfb8a867d21
Md.Mohiuddin <saddammd203@gmail.com>
2023-06-13 05:54:46 +0600

    added Google Login

--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -50,3 +50,4 @@ logOut
 };
 
 export default AuthProviders;
+
--- src/navItems/SignUp.jsx
+++ src/navItems/SignUp.jsx
@@ -1,71 +1,142 @@
+
 import { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { AuthContext } from "../Providers/AuthProviders";
 import { Link } from "react-router-dom";
 
-
-
 const SignUp = () => {
-    const { register, handleSubmit,  formState: { errors } } = useForm();
-const {createUser}=useContext(AuthContext);
-    const onSubmit = data => {
-        console.log(data)
-        createUser(data.email, data.password)
-        .then(result=>{
-            const loggedUser=result.user;
-            console.log(loggedUser);
-        })
-    };
-  
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm();
+  const { createUser } = useContext(AuthContext);
+  const onSubmit = (data) => {
+    console.log(data);
+    createUser(data.email, data.password).then((result) => {
+      const loggedUser = result.user;
+      console.log(loggedUser);
+    });
+  };
 
-    return (
-        <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">SignUp now!</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-          </div>
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Name</span>
-                </label>
-                <input type="text" {...register("name",{ required: true })} placeholder="name" className="input input-bordered" />
-                {errors.name && <span className="text-red-600">Name is required</span>}
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input type="email" {...register("email",{required:true})} placeholder="email" name="email" className="input input-bordered" />
-                {errors.email && <span className="text-red-600">Email is required</span>}
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input type="password" {...register("password",{required:true, 
-                minLength:6,
-                maxLength: 20,
-            pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z])/
-            })} placeholder="password" 
-                name="password" className="input input-bordered" />
-                {errors.password?.type === 'required' && <p className="text-red-600">password is required</p>}
-                {errors.password?.type === 'minLength' && <p className="text-red-600">Password must be 6 characters</p>}
-                {errors.password?.type === 'maxLength' && <p className="text-red-600">Password must be less than 20 characters</p>}
-                {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have one upper case, one lower case, one number and one special character</p>}
-                
-              </div>
-              <div className="form-control mt-6">
-                <input className="btn btn-primary" type="submit" value="Sign Up" />
-              </div>
-            </form>
-            <p><small>Already have an account  ? <Link to="/login">Login</Link></small></p>
-          </div>
+  const password = watch("password"); // Get the value of the password field
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">SignUp now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                {...register("name", { required: true })}
+                placeholder="Name"
+                className="input input-bordered"
+              />
+              {errors.name && (
+                <span className="text-red-600">Name is required</span>
+              )}
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                {...register("email", { required: true })}
+                placeholder="Email"
+                name="email"
+                className="input input-bordered"
+              />
+              {errors.email && (
+                <span className="text-red-600">Email is required</span>
+              )}
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                {...register("password", {
+                  required: true,
+                  minLength: 6,
+                  maxLength: 20,
+                  pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z])/,
+                })}
+                placeholder="Password"
+                name="password"
+                className="input input-bordered"
+              />
+              {errors.password?.type === "required" && (
+                <p className="text-red-600">Password is required</p>
+              )}
+              {errors.password?.type === "minLength" && (
+                <p className="text-red-600">Password must be 6 characters</p>
+              )}
+              {errors.password?.type === "maxLength" && (
+                <p className="text-red-600">Password must be less than 20 characters</p>
+              )}
+              {errors.password?.type === "pattern" && (
+                <p className="text-red-600">
+                  Password must have one uppercase, one lowercase, one number, and one special character
+                </p>
+              )}
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Confirm Password</span>
+              </label>
+              <input
+                type="password"
+                {...register("confirmPassword", {
+                  required: true,
+                  validate: (value) => value === password || "Passwords do not match",
+                })}
+                placeholder="Confirm Password"
+                name="confirmPassword"
+                className="input input-bordered"
+              />
+              {errors.confirmPassword && (
+                <p className="text-red-600">{errors.confirmPassword.message}</p>
+              )}
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Photo URL</span>
+              </label>
+              <input
+                type="text"
+                {...register("photoURL")}
+                placeholder="Photo URL"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control mt-6">
+              <input className="btn btn-primary" type="submit" value="Sign Up" />
+            </div>
+          </form>
+          <p>
+            <small>
+              Already have an account? <Link to="/login">Login</Link>
+            </small>
+          </p>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default SignUp;
