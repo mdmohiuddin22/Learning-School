b1933cef62aced872923d54934137d082135de3b
Md.Mohiuddin <saddammd203@gmail.com>
2023-06-12 15:37:06 +0600

    added login without header & footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,14 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../Nav+Footer/Navbar";
 import Footer from "../Nav+Footer/Footer";
 
 
 const Main = () => {
+  const location =useLocation();
+  console.log(location);
+  const noHeaderFooter =location.pathname.includes('login')
     return (
         <div>
-          <Navbar></Navbar>  
+         { noHeaderFooter || <Navbar></Navbar> } 
           <Outlet></Outlet>
-          <Footer></Footer>
+          { noHeaderFooter || <Footer></Footer>}
         </div>
     );
 };
--- src/Nav+Footer/Navbar.jsx
+++ src/Nav+Footer/Navbar.jsx
@@ -1,39 +1,44 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div className="navbar bg-green-400 sticky top-0 z-50">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-            </label>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              <li><a>Item 1</a></li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </li>
-              <li><a>Item 3</a></li>
-            </ul>
-          </div>
-          <a className="btn btn-ghost normal-case text-xl font-bold ">ARABIC  LEARNING    SCHOOL</a>
-        </div>
-        <div className="navbar-center hidden lg:flex text-lg font-semibold">
-          <ul className="menu menu-horizontal px-1">
-           <Link to="/"> <li><a>Home</a></li></Link>
-            <Link to="/instructors"><li><a>Instructors</a></li></Link>
-            <Link to="classes"><li><a>Classes</a></li></Link>
-            <Link to="dashboard"><li><a>Dashboard</a></li></Link>
-            <Link to="login"><li><a>Login</a></li></Link>
+  return (
+    <div className="navbar bg-green-400 sticky top-0 z-50">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          </label>
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            <li><a>Item 1</a></li>
+            <li>
+              <a>Parent</a>
+              <ul className="p-2">
+                <li><a>Submenu 1</a></li>
+                <li><a>Submenu 2</a></li>
+              </ul>
+            </li>
+            <li><a>Item 3</a></li>
           </ul>
         </div>
-
+        <a className="btn btn-ghost normal-case text-xl font-bold ">ARABIC  LEARNING    SCHOOL</a>
       </div>
-    );
+      <div className="navbar-center hidden lg:flex text-lg font-semibold">
+        <ul className="menu menu-horizontal px-1">
+          <Link to="/"> <li><a>Home</a></li></Link>
+          <Link to="/instructors"><li><a>Instructors</a></li></Link>
+          <Link to="classes"><li><a>Classes</a></li></Link>
+          <Link to="dashboard"><li><a>Dashboard</a></li></Link>
+          <Link to="login"><li><a>Login</a></li></Link>
+        </ul>
+      </div>
+
+    </div>
+  );
 };
 
 export default Navbar;
+
+
+
+
+
