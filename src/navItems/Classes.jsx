213d1b02fc3a6baf01f47adb35aed9017f393b74
Md.Mohiuddin <saddammd203@gmail.com>
2023-06-13 20:33:18 +0600

    add classes

--- src/navItems/Classes.jsx
+++ src/navItems/Classes.jsx
@@ -1,10 +1,41 @@
 
-const Classes = () => {
-    return (
-        <div>
-            <h1>This is Classes</h1>
-        </div>
-    );
+
+const ArabicLearningPage = () => {
+  // Sample data for classes
+  const classes = [
+    { id: 1, name: 'Class A', students: 30, image: 'https://i.ibb.co/vsKDDFf/teacher-7735596-1280.png' },
+    { id: 2, name: 'Class B', students: 20, image: 'https://i.ibb.co/bPvQkq1/religion-40578-1280.png' },
+    { id: 3, name: 'Class C', students: 15, image: 'https://i.ibb.co/0Q0ZQMY/arab-muslim-family-traditional-clothing-with-allahu-akbar-1308-48924.jpg' },
+    { id: 4, name: 'Class D', students: 25, image: 'https://i.ibb.co/0Q0ZQMY/arab-muslim-family-traditional-clothing-with-allahu-akbar-1308-48924.jpg' },
+    { id: 5, name: 'Class E', students: 40, image: 'https://i.ibb.co/x8v47HK/realistic-ramadan-kareem-illustration-23-2148871084.jpg' },
+    { id: 6, name: 'Class F', students: 35, image: 'https://i.ibb.co/xJGKQMQ/ramadan-saudi-arabia-islamic-background-187299-36899.jpg' },
+    { id: 7, name: 'Class G', students: 18, image: 'https://i.ibb.co/q9sT2ZR/name-allah-arab-lettering-23-2148679791.jpg' },
+    { id: 8, name: 'Class H', students: 22, image: 'https://i.ibb.co/Nrs1nhf/arabic-alphabet-worksheet-vector-design-arabic-letters-children-s-learning-write-642050-195.jpg' },
+    // Add more classes as needed
+  ];
+
+  // Sort the classes based on the number of students
+  const sortedClasses = [...classes].sort((a, b) => b.students - a.students);
+
+  // Get the top 6 classes
+  const topClasses = sortedClasses.slice(0, 6);
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl font-bold mb-8">All Classes</h1>
+
+      {/* Display relevant pictures */}
+      <div className="grid grid-cols-3 gap-4">
+        {topClasses.map((classItem) => (
+          <div key={classItem.id} className="bg-gray-200 p-4">
+            <img src={classItem.image} alt={classItem.name} className="w-full h-40 object-cover mb-2" />
+            <h2 className="text-lg font-semibold mb-2">{classItem.name}</h2>
+            <p>{classItem.students} students</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default Classes;
+export default ArabicLearningPage;
--- src/navItems/Instructors.jsx
+++ src/navItems/Instructors.jsx
@@ -1,11 +1,37 @@
 
 
-const Instructors = () => {
-    return (
-        <div>
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum placeat iste tempore! Facilis quas iure soluta nulla. Molestias, sapiente libero.</h1>
+const PopularInstructors = () => {
+  // Dummy data for instructors (replace with actual data)
+  const instructors = [
+    { name: 'Abu Sayed', students: 100, image: 'https://i.ibb.co/FD9Kqfw/young-bearded-man-glasses-blue-shirt-holding-book-smiling-confident-141793-37440.jpg' },
+    { name: 'Ibne Khaldun', students: 90, image: 'https://i.ibb.co/4KvCmV7/businessman-black-suit-holding-his-tasklist-makes-thumb-up-114579-15902.jpg' },
+    { name: 'Mohi Uddin', students: 80, image: 'https://i.ibb.co/R3zg0t8/arabian-saudi-businessman-155003-19336.jpg' },
+    { name: 'Al Amin', students: 70, image: 'https://i.ibb.co/MRQ4Srp/pleased-young-businessman-wearing-glasses-holding-book-isolated-white-background-141793-63613.jpg' },
+    { name: 'Mahbubur Rahman', students: 60, image: 'https://i.ibb.co/k9ZynY2/arab-man-cartoon-character-1308-50653.jpg' },
+    { name: 'Ibne Botuta', students: 50, image: 'https://i.ibb.co/5jF79k5/teacher-brunette-instructor-with-computer-suit-whiteboard-classroom-confident-140725-163241.jpg' },
+  ];
+
+  // Sort instructors based on the number of students (optional)
+  const sortedInstructors = instructors.sort((a, b) => b.students - a.students);
+
+  return (
+    <div className="flex justify-center">
+      <div className="max-w-lg">
+        <h2 className="text-3xl font-bold mb-4">Popular Instructors</h2>
+        <div className="grid grid-cols-2 gap-4">
+          {sortedInstructors.slice(0, 6).map((instructor) => (
+            <div key={instructor.name} className="bg-white rounded-lg shadow-md">
+              <img src={instructor.image} alt={instructor.name} className="w-full h-48 object-cover rounded-t-lg" />
+              <div className="p-4">
+                <h3 className="text-lg font-semibold mb-2">{instructor.name}</h3>
+                <p className="text-gray-500">{instructor.students} students</p>
+              </div>
+            </div>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
-export default Instructors;
+export default PopularInstructors;
