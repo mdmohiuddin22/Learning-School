148abff8d8e993c9e6d17e187cc2d90de7ba7b3e
Md.Mohiuddin <saddammd203@gmail.com>
2023-08-03 09:55:17 +0600

    modify instructors

--- src/navItems/Instructors.jsx
+++ src/navItems/Instructors.jsx
@@ -3,12 +3,12 @@
 const PopularInstructors = () => {
   
   const instructors = [
-    { name: 'Abu Sayed', students: 100, image: 'https://i.ibb.co/nR3VwcZ/Teacher-1.jpg' },
-    { name: 'Ibne Khaldun', students: 90, image: 'https://i.ibb.co/4KvCmV7/businessman-black-suit-holding-his-tasklist-makes-thumb-up-114579-15902.jpg' },
-    { name: 'Mohi Uddin', students: 80, image: 'https://i.ibb.co/R3zg0t8/arabian-saudi-businessman-155003-19336.jpg' },
-    { name: 'Al Amin', students: 70, image: 'https://i.ibb.co/MRQ4Srp/pleased-young-businessman-wearing-glasses-holding-book-isolated-white-background-141793-63613.jpg' },
-    { name: 'Mahbubur Rahman', students: 60, image: 'https://i.ibb.co/k9ZynY2/arab-man-cartoon-character-1308-50653.jpg' },
-    { name: 'Ibne Botuta', students: 50, image: 'https://i.ibb.co/5jF79k5/teacher-brunette-instructor-with-computer-suit-whiteboard-classroom-confident-140725-163241.jpg' },
+    { name: 'Abu Sayed',email:'abusayed@gmail.com', students: 100, image: 'https://i.ibb.co/nR3VwcZ/Teacher-1.jpg' },
+    { name: 'Ibne Khaldun',email:'ibnekhaldun@gmail.com', students: 90, image: 'https://i.ibb.co/4KvCmV7/businessman-black-suit-holding-his-tasklist-makes-thumb-up-114579-15902.jpg' },
+    { name: 'Mohi Uddin',email:'mohiuddin@gmail.com', students: 80, image: 'https://i.ibb.co/R3zg0t8/arabian-saudi-businessman-155003-19336.jpg' },
+    { name: 'Al Amin',email:'alamin@gmail.com', students: 70, image: 'https://i.ibb.co/MRQ4Srp/pleased-young-businessman-wearing-glasses-holding-book-isolated-white-background-141793-63613.jpg' },
+    { name: 'Mahbubur Rahman',email:'mahbuburrahman@gmail.com', students: 60, image: 'https://i.ibb.co/k9ZynY2/arab-man-cartoon-character-1308-50653.jpg' },
+    { name: 'Ibne Botuta',email:'ibnebotuta@gmail.com', students: 50, image: 'https://i.ibb.co/5jF79k5/teacher-brunette-instructor-with-computer-suit-whiteboard-classroom-confident-140725-163241.jpg' },
   ];
 
 
@@ -31,6 +31,7 @@ const PopularInstructors = () => {
               {/* <img className="rounded-lg w-64" src={instructor.image} alt={instructor.name}  /> */}
               <div className="p-4">
                 <h3 className="text-lg font-semibold mb-2 ">{instructor.name}</h3>
+                <h3>Email: {instructor.email}</h3>
                 <p className="text-gray-500">{instructor.students} students</p>
               </div>
             </div>
